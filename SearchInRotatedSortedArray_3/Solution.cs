4bfad7cf0252d13cb8b8d7a15f86b0fe8aac9378
KolesovDI <den.kolesov@gmail.com>
2025-10-31 16:05:54 +0300

    (Binary Search) 3 Searc In Rotated Sorted Array

---
+++ SearchInRotatedSortedArray_3/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInRotatedSortedArray_3
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Linear Search
+        /// O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchLinear(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Binary Search
+        /// O(log(n))
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="terget"></param>
+        /// <returns></returns>
+        public int Search(int[] nums, int terget)
+        {
+            int left = 0, right = nums.Length - 1;
+
+
+        }
+    }
+}

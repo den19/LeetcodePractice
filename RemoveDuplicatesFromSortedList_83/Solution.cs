607e45f3359c73e818e5708d24d0567494967669
KolesovDI <den.kolesov@gmail.com>
2025-10-31 06:07:16 +0300

    (Linked List) 83 Remove Duplicates From Sorted List

---
+++ RemoveDuplicatesFromSortedList_83/Solution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesFromSortedList_83
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// Ordinary method for remove duplicates
+        /// O(n)
+        /// O(1)
+        /// Optimal algorithm
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if(head == null || head.next == null)
+            {
+                return head;
+            }
+
+            var current = head;
+
+            while (current != null && current.next != null)
+            {
+                if(current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+
+        /// <summary>
+        /// Optimized approach using HashSet
+        /// O(n)
+        /// O(k), k - number of unique elemnts in HasSet
+        /// Not optimal algorithm compared to previous
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DeleteDuplicatesSet(ListNode head)
+        {
+            var seenValues = new HashSet<int>();
+            var dummyHead = new ListNode(-1);
+            dummyHead.next = head;
+            var prev = dummyHead;
+
+            while (head != null)
+            {
+                if (seenValues.Contains(head.val))
+                {
+                    prev.next = head.next;
+                }
+                else
+                {
+                    seenValues.Add(head.val);
+                    prev = head;
+                }
+                head = head.next;
+            }
+
+            return dummyHead.next;
+        }
+    }
+
+
+}
---
+++ RemoveDuplicatesFromSortedList_83_Test/Tests.cs
@@ -0,0 +1,66 @@
+﻿using RemoveDuplicatesFromSortedList_83;
+
+namespace RemoveDuplicatesFromSortedList_83_Test
+{
+    public class Tests
+    {
+        public class RemoveDuplicatesTests
+        {
+
+            private static void AssertListEqual(ListNode expected, ListNode actual)
+            {
+                while (expected != null && actual != null)
+                {
+                    Assert.Equal(expected.val, actual.val);
+                    expected = expected.next;
+                    actual = actual.next;
+                }
+                // Both list must end simulteniously
+                Assert.Null(expected);
+                Assert.Null(actual);
+            }
+
+            [Fact]
+            public void Test_RemoveDuplicatesFromSortedList()
+            {
+                // Arrange
+                var input = new ListNode(1, new ListNode(1, new ListNode(2)));
+                var solution = new Solution();
+
+                // Act
+                var result = solution.DeleteDuplicates(input);
+
+                // Assert
+                AssertListEqual(new ListNode(1, new ListNode(2)), result);
+            }
+
+            [Fact]
+            public void Test_RemoveAllDuplicates()
+            {
+                // Arrange
+                var input = new ListNode(1, new ListNode(1));
+                var solution = new Solution();
+
+                // Act
+                var result = solution.DeleteDuplicates(input);
+
+                // Assert
+                AssertListEqual(new ListNode(1), result);
+            }
+
+            [Fact]
+            public void Test_EmptyList()
+            {
+                // Arrange
+                ListNode input = null;
+                var solution = new Solution();
+
+                // Act
+                var result = solution.DeleteDuplicates(input);
+
+                // Assert
+                Assert.Null(result);
+            }
+        }
+    }
+}

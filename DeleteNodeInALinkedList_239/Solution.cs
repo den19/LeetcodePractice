b27dd85767ead4b2824dc6b53c79eedb229dc3df
KolesovDI <den.kolesov@gmail.com>
2025-11-03 09:00:49 +0300

    (Linked List Easy) 237 Delete Node In A Linked List

---
+++ DeleteNodeInALinkedList_239/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace DeleteNodeInALinkedList_239
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// Efficient solution
+        /// O(1)
+        /// O(1)
+        /// </summary>
+        /// <param name="node"></param>
+        public void DeleteNode(ref ListNode node)
+        {
+            if(node.next == null)
+            {
+                node = null;
+                return;
+            }
+            node.val = node.next.val;
+
+            node.next = node.next.next;
+        }
+    }
+}
---
+++ DeleteNodeInALinkedList_239_Test/Tests.cs
@@ -0,0 +1,89 @@
+﻿using DeleteNodeInALinkedList_239;
+
+namespace DeleteNodeInALinkedList_239_Test
+{
+    public class DeleteNodeTests
+    {
+        private readonly Solution _solution = new Solution();
+
+        private static ListNode CreateList(params int[] values)
+        {
+            if (values.Length == 0) return null;
+
+            var head = new ListNode(values[0]);
+            var current = head;
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+
+            return head;
+        }
+
+        private static string ToString(ListNode node)
+        {
+            var result = "";
+            while (node != null)
+            {
+                result += $"{node.val}, ";
+                node = node.next;
+            }
+            return result.TrimEnd(',', ' ');
+        }
+
+        [Fact]
+        public void Test_DeleteMiddleNode()
+        {
+            // Arrange
+            var head = CreateList(1, 2, 3, 4);
+            var targetNode = head.next; // Удаляем второй элемент
+
+            // Act
+            _solution.DeleteNode(ref targetNode); // Вызываем метод удаления
+
+            // Assert
+            Assert.Equal("1, 3, 4", ToString(head));
+        }
+
+        [Fact]
+        public void Test_DeleteFirstNode()
+        {
+            // Arrange
+            var head = CreateList(1, 2, 3);
+            var targetNode = head; // Удаляем первый элемент
+
+            // Act
+            _solution.DeleteNode(ref targetNode); // Вызываем метод удаления
+
+            // Assert
+            Assert.Equal("2, 3", ToString(head));
+        }
+
+        [Fact]
+        public void Test_SingleElementList()
+        {
+            // Arrange
+            var head = CreateList(1);
+            var targetNode = head; // Единственный узел
+
+            // Act
+            _solution.DeleteNode(ref head); // Вызываем метод удаления
+
+            // Assert
+            Assert.Null(head); // После удаления список пуст
+        }
+
+        [Fact]
+        public void Test_EmptyList()
+        {
+            // Arrange
+            var head = CreateList();
+            var targetNode = head; // Список пустой
+
+            // Act & Assert
+            Assert.ThrowsAny<Exception>(() => _solution.DeleteNode(ref targetNode)); // Ожидаем исключение, так как удалять нечего
+        }
+    }
+}

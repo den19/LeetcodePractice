f50c32a5c0cd4bfd664f866f38ddb494f60770b6
KolesovDI <den.kolesov@gmail.com>
2025-11-05 05:45:22 +0300

    (Linked List Easy) 706 Design HashMap

--- DesignHashMap_706/Solution.cs
+++ DesignHashMap_706/Solution.cs
@@ -1,4 +1,6 @@
-﻿namespace DesignHashMap_706
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesignHashMap_706
 {
     public class Solution
     {
@@ -11,11 +13,11 @@
     /// Average time complexity O(1)
     /// Memory complexity O(m +n), n - number of elements, m - array size
     /// </summary>
-    public class HashSetArray
+    public class HashMapArray
     {
         private List<(int key, int val)>[] data;
 
-        public HashSetArray() 
+        public HashMapArray() 
         {
             // Length can be optimized
             data = new List<(int key, int val)>[1000];
@@ -83,4 +85,127 @@
     }
 
 
+    /// <summary>
+    /// Optimized solution with dynamic capacity growth
+    /// Time of inserting and search is near O(1)
+    /// </summary>
+    public class MyHashMap
+    {
+        private const double LOAD_FACTOR_THRESHOLD = 0.75d;
+
+        private Entry[] entries;
+
+        private int size;
+
+        public MyHashMap() : this(8)
+        {
+        }
+
+        public MyHashMap(int capacty)
+        {
+            entries = new Entry[capacty];
+        }
+
+        public void Put(int key, int value)
+        {
+            RehashIfNeeded();
+
+            var index = FindEntryIndex(key);
+
+            if (entries[index] == null || entries[index].Key == -1)
+            {
+                entries[index] = new Entry(key, value);
+                size++;
+            }
+            else
+            {
+                entries[index].Value = value;
+            }
+        }
+
+        public int Get(int key)
+        {
+            var index = FindEntryIndex(key);
+
+            if (entries[index] != null && entries[index].Key >= 0)
+            {
+                return entries[index].Value;
+            }
+
+            return -1;
+        }
+
+        public void Remove(int key)
+        {
+            var index = FindEntryIndex(key);
+
+            if (entries[index] != null && entries[index].Key >= 0)
+            {
+                // Mark cell as deleted
+                entries[index].MarkAsDeleted();
+                size--;
+            }
+        }
+
+        private bool NeedRehash() => ((double) size / entries.Length) > LOAD_FACTOR_THRESHOLD;
+
+        private void RehashIfNeeded()
+        {
+            if (NeedRehash())
+            {
+                var oldEntries = entries.ToArray();
+                // Increase capacity two times
+                entries = new Entry[oldEntries.Length * 2];
+
+                size = 0;
+
+                foreach(var entry in oldEntries)
+                {
+                    if(entry != null && entry.Key >= 0)
+                    {
+                        // Rearrange old records
+                        Put(entry.Key, entry.Value);
+                    }
+                }
+            }
+        }
+
+        private int FindEntryIndex(int key)
+        {
+            var hash = GetHash(key);
+
+            while (true)
+            {
+                if (entries[hash] == null || entries[hash].Key == key)
+                {
+                    return hash;
+                }
+
+                // Goto next free cell
+            }
+        }
+
+        private int GetHash(int key) => Math.Abs(key % entries.Length);
+
+        private int ProbeNext(int currentIndex) => (currentIndex + 1) % entries.Length;
+    }
+
+    class Entry
+    {
+        public int Key { get; set; }
+
+        public int Value { get; set; }
+
+        public Entry(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public void MarkAsDeleted()
+        {
+            // Mark as deleted
+            Key = -1;
+        }
+    }
 }

6074d90b3c673741c77f3e5feb82b5c56df6fbd2
KolesovDI <den.kolesov@gmail.com>
2025-11-07 06:23:12 +0300

    (Linked List Easy) 1290 Convert Binary Number In A Linked List To Integer Tests

---
+++ ConvertBinaryNumberInALinkedListToInteger_1290_Test/Tests.cs
@@ -0,0 +1,48 @@
+﻿using ConvertBinaryNumberInALinkedListToInteger;
+
+namespace ConvertBinaryNumberInALinkedListToInteger_1290_Test
+{
+    public class TestSolution
+    {
+
+        private readonly Solution _solution = new Solution();
+
+        [Fact]
+        public void SingleBitTest()
+        {
+            var node = new ListNode(1);   // Представляет '1' в двоичном виде
+            Assert.Equal(1, _solution.GetDecimalValueEffective(node));
+        }
+
+        [Fact]
+        public void SimpleBinaryTest()
+        {
+            var node = new ListNode(1, new ListNode(0));  // Представляет '10' (или 2 в десятичном виде)
+            Assert.Equal(2, _solution.GetDecimalValueEffective(node));
+        }
+
+        [Fact]
+        public void ComplexBinaryTest()
+        {
+            var node = new ListNode(1, new ListNode(0, new ListNode(1)));  // Представляет '101' (или 5 в десятичном виде)
+            Assert.Equal(5, _solution.GetDecimalValueEffective(node));
+        }
+
+        [Fact]
+        public void AllZerosTest()
+        {
+            var node = new ListNode(0, new ListNode(0, new ListNode(0)));
+            Assert.Equal(0, _solution.GetDecimalValueEffective(node));
+        }
+
+        [Fact]
+        public void LongListTest()
+        {
+            var node = new ListNode(1,
+                        new ListNode(0,
+                            new ListNode(1,
+                                new ListNode(1))));  // Представляет '1011' (или 11 в десятичном виде)
+            Assert.Equal(11, _solution.GetDecimalValueEffective(node));
+        }
+    }
+}

9f2287f05913b573c98bc7b0db9072bc1a4deb7f
KolesovDI <den.kolesov@gmail.com>
2025-11-07 05:48:32 +0300

    (Linked List Easy) 1290 Convert Binary Number In A Linked List To Integer

---
+++ ConvertBinaryNumberInALinkedListToInteger/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace ConvertBinaryNumberInALinkedListToInteger
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// Decision though string of binary values
+        /// O(n)
+        /// O(n), because length of string equals length of linked list
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public int GetDecimalValueLinear(ListNode head)
+        {
+            // Create empty string for storing binary values
+            string binaryString = string.Empty;
+
+            while (head != null)
+            {
+                // Add current binary value to the end of the string
+                binaryString += head.val.ToString();
+
+                head = head.next;
+            }
+
+            // Convert binary string to integer
+            return Convert.ToInt32(binaryString, 2);
+        }
+    }
+}

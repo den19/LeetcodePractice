fba21cbb01859e2b2ebc4b5d171af539f4541ce8
Denis Kolesov <denis.kolesov@cdtek.ru>
2025-11-15 07:26:03 +0300

    (Leetcode Easy) 3063 LinkedListFrequency

---
+++ LinkedListFrequency_3063/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListFrequency_3063
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LinkedListFrequency_3063/Solution.cs
@@ -0,0 +1,84 @@
+﻿namespace LinkedListFrequency_3063
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// Optimized Hash Map Approach
+        /// O(n)
+        /// O(n)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode FrequencyOptimized(ListNode head)
+        {
+            if (head == null) return null;
+
+            // Dictionary to store frequency counts
+
+        }
+
+        public ListNode FrequencyBruteForce(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode dummy = new ListNode();
+            ListNode currentNew = dummy;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                int value = current.val;
+                int count = 0;
+
+                // Count frequency of current value
+                ListNode temp = head;
+                while (temp != null)
+                {
+                    if(temp.val == value)
+                    {
+                        count++;
+                    }
+                    temp = temp.next;
+                }
+
+                // Check if we already prcessed this value
+                bool alreadyProcessed = false;
+                ListNode check = head;
+                while (check != current)
+                {
+                    if (check.val == value)
+                    {
+                        alreadyProcessed = true;
+                        break;
+                    }
+                    check = check.next;
+                }
+
+                //Add to result if first occurrence
+                if (!alreadyProcessed)
+                {
+                    currentNew.next = new ListNode(count);
+                    currentNew = currentNew.next;
+                }
+                current = current.next;
+            }
+
+            return dummy.next;
+        }
+    }
+}

20b5d5a9009ab1d079ce20df5f7c35162903ee01
KolesovDI <den.kolesov@gmail.com>
2025-11-18 05:09:16 +0300

    (Trees Easy) 222 Count Complete Tree Nodes

---
+++ CountCompleteTreeNodes_222_Medium/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CountCompleteTreeNodes_222_Medium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CountCompleteTreeNodes_222_Medium/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace CountCompleteTreeNodes_222
+{
+    public class Solution
+    {
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        /// <summary>
+        /// Linear approach - visit every node
+        /// Time O(N), N - number of nodes
+        /// Memory O(H), H - height of the tree (recursion stack)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int CountNodesLinear(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return 1 + CountNodesLinear(root.left) + CountNodesLinear(root.right);
+        }
+    }
+}

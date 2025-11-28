55f11aa5846ef30ab7966b530287e5f19372ae2d
Denis Kolesov <denis.kolesov@cdtek.ru>
2025-11-28 16:28:09 +0300

    (Linked List Medium) Start Rotate List (61)

---
+++ RotateList_61/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RotateList_61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ RotateList_61/Solutions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotateList_61
+{
+    public class Solutions
+    {
+    }
+}

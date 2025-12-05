05c3087789216a71f256803cd364700d659214ee
Den Kolesov <den.kolesov@gmail.com>
2025-12-05 08:13:30 +0300

    (Tree Easy) Balanced Binary Tree (110) tech

---
+++ BalancedBinaryTree_110/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBinaryTree_110
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class SolutionBruteForce
+    {
+        private int GetHeight(TreeNode node)
+        {
+            if(node == null) return 0;
+
+            return 1 + Math.Max(GetHeight(node.left),
+                                GetHeight(node.right));
+        }
+
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null) return true;
+
+            // 
+        }
+    }
+
+    public class SolutionDFSOptimal
+    {
+
+    }
+}

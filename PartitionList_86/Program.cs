7c6647ed4f8e7b7ab2316db6b839b0536729b9a1
Den Kolesov <den.kolesov@gmail.com>
2025-12-07 04:40:33 +0300

    (Linked List Medium) Partition List (86) BF Solution

---
+++ PartitionList_86/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PartitionList_86
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ PartitionList_86/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartitionList_86
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class SolutionBruteForce
+    {
+        /// <summary>
+        /// Brute force solution
+        /// O(n^2)
+        /// O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode dummy = new ListNode(0, head);
+            ListNode current = dummy;
+
+            while (current.next != null && current.next.val < x)
+            {
+                current = current.next;
+            }
+
+            ListNode insertPos = current;
+            current = current.next;
+
+            while (current != null && current.next != null)
+            {
+                if (current.next.val < x)
+                {
+                    // Удаляем узел с значением < x из текущей позиции
+                    ListNode smaller = current.next;
+                    current.next = current.next.next;
+
+                    // Вставляем его после insertPos
+                    smaller.next = insertPos.next;
+                    insertPos.next = smaller;
+                    insertPos = insertPos.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return dummy.next;
+        }
+    }
+}

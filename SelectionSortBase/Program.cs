e8c6f5d80cbfaf2b6947e1cd7170f632fccd1188
Denis Kolesov <denis.kolesov@cdtek.ru>
2025-12-16 12:23:26 +0300

    Selection Sort

---
+++ SelectionSortBase/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SelectionSortBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 64, 25, 12, 22, 11 };
+
+            Console.WriteLine("Исходный массив:");
+
+            
+            SelectionSort.PrintArray(numbers);
+
+            SelectionSort.Sort(numbers);
+
+            Console.WriteLine("\nОтсортированный массив:");
+            SelectionSort.PrintArray(numbers);
+        }
+    }
+}
---
+++ SelectionSortBase/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class SelectionSort
+{
+    // Основной метод сортировки выбором
+    public static void Sort(int[] array)
+    {
+        int n = array.Length;
+
+        // Проходим по всем элементам массива, кроме последнего
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Предполагаем, что минимальный элемент - текущий
+            int minIndex = i;
+
+            // Ищем минимальный элемент в оставшейся части массива
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            // Если нашли элемент меньше текущего, меняем их местами
+            if (minIndex != i)
+            {
+                // Обмен элементов
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
